9c93b280fa30837978827d50b89f89fd64b7770b
Jose Eduardo Vazquez Cortez <50307754+JoseCortez1@users.noreply.github.com>
2024-11-10 14:41:00 -0600

    style: fix responsive design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,15 +8,15 @@ import Button from "./Button";
 const Hero = () => {
 	return (
 		<div className='grid grid-cols-1 lg:grid-cols-2 lg:items-center min-h-[calc(100dvh-_var(--h-header))] '>
-			<div className='self-end lg:self-center pb-8 mt-[40%] mb-auto w-[150%] z-[1] '>
-				<h2 className='text-2xl sm:text-4xl text-white font-semibold  mb-8 leading-normal'>
+			<div className='self-end  lg:self-center pb-8 mt-4  lg:mt-[40%] lg:mb-auto lg:w-[150%] z-[1] '>
+				<h2 className='text-lg sm:text-2xl md:text-4xl text-white font-semibold mb-4 lg:mb-8 leading-normal'>
 					[Eduardo |
 					<strong className='text-secondary '>
 						Next.js & Nuxt.js
 					</strong>{" "}
 					Frontend Developer]
 				</h2>
-				<p className='text-gray-300  '>
+				<p className='text-gray-300 text-xs md:text-base  '>
 					[ develop modern and high-performance web applications using
 					the best tools from React and Vue ecosystems. My expertise
 					with Next.js and Nuxt.js more...
@@ -24,18 +24,18 @@ const Hero = () => {
 					SEO-optimized web experiences, always focused on maintaining
 					the highest standards of quality and performance."] */}
 				</p>
-				<Button extraClassName='my-8'>Contact me!!</Button>
+				<Button extraClassName='my-4 lg:my-8'>Contact me!!</Button>
 			</div>
-			<div className='relative flex flex-col justify-end min-h-[40vh] mb-[30%] mt-auto'>
+			<div className='relative flex flex-col justify-end min-h-[40vh] lg:mb-[30%] mt-auto'>
 				<Image
 					src={logoOutline}
 					alt='Logo outline'
-					className='absolute bottom-10 left-1/2 w-[23rem] -translate-x-1/2 '
+					className='absolute bottom-10 left-1/2 w-[14rem] sm:w-[23rem] -translate-x-1/2 '
 				/>
 				<Image
 					src={dottedBox}
 					alt='Dotted box'
-					className='absolute bottom-[10rem] right-[4rem]  w-[8rem] '
+					className='absolute bottom-[10rem] right-[4rem] w-[4rem] sm:w-[8rem] '
 				/>
 
 				<p className='border absolute bottom-10 w-full border-gray-500 flex justify-center  items-center z-[1] bg-primary/70  text-sm py-10'>
