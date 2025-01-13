ae436bad8e6152458dc9f7245f32a61b63c88728
Eduardo Cortez <50307754+JoseCortez1@users.noreply.github.com>
2025-01-12 19:00:10 -0600

    fieat: add responsive design to all sections

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import logoOutline from "../../resources/svg/logo-outline.svg";
 import { CURRENT_PROYECT } from "../../lib/globals";
-import Button from "../ui/button";
+import Button from "../ui/Button";
 
 const Hero = () => {
 	return (
 		<div className="relative grid grid-cols-1 lg:grid-cols-2 lg:items-center min-h-[calc(100dvh-_var(--h-header))] ">
-			<div className="self-end  lg:self-center pb-8 mt-4  lg:mt-[40%] lg:mb-auto lg:w-[150%] z-[1] ">
+			<div className="self-end  lg:self-center pb-8 mt-4  lg:mt-[40%] lg:mb-auto lg:w-[150%] z-[1]  bg-primary/70 md:bg-transparent">
 				<h2 className="text-lg sm:text-2xl md:text-4xl text-white font-semibold mb-4 lg:mb-8 leading-normal">
 					[Eduardo |
 					<strong className="text-secondary ">Next.js & Nuxt.js</strong>{" "}
 					Frontend Developer]
 				</h2>
-				<p className="text-gray-300 text-xs md:text-base  ">
+				<p className="text-gray-300 text-xs md:text-base  my-20 md:my-10">
 					[ develop modern and high-performance web applications using the best
 					tools from React and Vue ecosystems. My expertise with Next.js and
 					Nuxt.js more...
@@ -24,13 +24,13 @@ const Hero = () => {
 				</p>
 				<Button className="my-4 lg:my-8">Contact me!!</Button>
 			</div>
-			<div className="relative flex flex-col justify-end min-h-[40vh] lg:mb-[30%] mt-auto">
+			<div className="relative flex flex-col justify-end h-full min-h-[40vh] lg:mb-[30%] mt-auto ">
 				<Image
 					src={logoOutline}
 					alt="Logo outline"
-					className="absolute bottom-10 grayscale-[0.1] left-1/2 w-full sm:h-[70vh] -translate-x-1/2 translate-y-1/4 "
+					className="absolute bottom-10 blur-sm md:blur-none md:grayscale-[0.1] left-1/2 w-full h-[60vh]  md:h-[70vh] -translate-x-1/2 -translate-y-1/4  md:translate-y-0 "
 				/>
-
+				<div className="absolute top-0 bottom-0 left-0 right-0 bg-primary/70 md:hidden"></div>
 				<p className="border absolute bottom-10  lg:-bottom-1/2 w-full border-gray-500 flex justify-center  items-center z-[1] bg-primary/70  text-sm py-10">
 					<span className="bg-secondary inline-block w-4 h-4 mx-2 flex-shrink-0"></span>
 					[Current working on{" "}
